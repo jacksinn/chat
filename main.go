dbc3ff1cfc85b56cf1f2acc7ae9257dd80706f6a
Steven <scjackson@linux.com>
2017-05-01 12:45:39 -0400

    loading templates

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
+	"html/template"
+	"path/filepath"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte(`<html>
-		<head><title>Chat</title></head>
-		<body>
-		Let's chat!
-		</body>`))
+//templ represents a single template
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+//ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	if err := http.ListenAndServe(":8080", nil);
 	err != nil {
 		log.Fatal("Listen and serve:", err)
